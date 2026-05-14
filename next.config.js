53945402e3eaa2e14a761e7ff486b47e6c5b6d14
hwnsng <amc214677@gmail.com>
2026-05-14 16:11:09 +0900

    feat: HTTPS 문제 우회하는 설정 추가

--- next.config.js
+++ next.config.js
@@ -1,2 +1,13 @@
 /** @type {import('next').NextConfig} */
-module.exports = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://13.209.18.148:8080/:path*",
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
